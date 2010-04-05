2cddf4f64ed7d1303ffaad63ca1bdee9e32781fb
ashmore <ashmore@d15e2e6f-3fd1-c693-ec5a-e8bef22e7ec3>
2010-04-05 17:40:11 +0000

    Includes word bubbles and speech.

--- trunk/src/main/Main.java
+++ trunk/src/main/Main.java
@@ -4,6 +4,7 @@
  */
 package main;
 
+import java.util.Random;
 import processing.core.PApplet;
 import testworld.Testworld;
 import utils.math.Vector2d;
@@ -20,7 +21,6 @@ public class Main extends PApplet {
     public static void main(String[] args) {
         PApplet.main(new String[]{"--bgcolor=#AF8F92", "main.Main"});
     }
-
     private Testworld world;
 
     public Main() {
@@ -30,6 +30,7 @@ public class Main extends PApplet {
     @Override
     public void setup() {
         size(500, 500);
+        smooth();
     }
 
     @Override
@@ -43,5 +44,21 @@ public class Main extends PApplet {
         world.getPlayer().forceMoveTo(new Vector2d(mouseX, mouseY));
     }
 
+    @Override
+    public void keyTyped() {
+        if (key == 'g') {
 
+            String[] thingsToSay = new String[]{
+                "Have we met somewhere before?",
+                "I think the host is a real bore.",
+                "Does the pate taste funny to you?",
+                "Nice jacket!",
+                "No, do go on. That's terribly fascinating.",
+                "I see that you're certainly enjoying yourself.",
+                "..."};
+
+            int index = new Random().nextInt(thingsToSay.length);
+            world.getPlayer().pushSpeech(thingsToSay[index]);
+        }
+    }
 }
---
+++ trunk/src/testworld/representations/WordBubble.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testworld.representations;
+
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+/**
+ *
+ * @author Calvin Ashmore
+ */
+public class WordBubble {
+
+    private static final int MS_PER_CHARACTER = 75;
+    private static final int BASE_OFFSET = 500;
+
+    private String text;
+    private long expireTime;
+
+    public WordBubble(String text) {
+        this.text = text;
+        expireTime = System.currentTimeMillis() + BASE_OFFSET + MS_PER_CHARACTER * text.length();
+    }
+
+    public void render(PGraphics g) {
+
+        int textSize = 13;
+        int padding = 2;
+
+        float textWidth = g.textWidth(text);
+
+        g.pushMatrix();
+        g.translate(0, -textSize - padding - 10 - 35);
+
+        g.textSize(textSize);
+        g.fill(255f);
+        g.stroke(0f);
+        g.rect(-padding - textWidth / 2, -padding, textWidth + 2 * padding, textSize + 2 * padding);
+
+        g.stroke(255f);
+        g.line(5, textSize + padding, 9, textSize + padding);
+
+        g.noStroke();
+        g.triangle(5, textSize + padding, 0, textSize + padding + 10, 10, textSize + padding);
+        g.stroke(0f);
+        g.line(5, textSize + padding, 0, textSize + padding + 10);
+        g.line(10, textSize + padding, 0, textSize + padding + 10);
+
+        g.fill(0f);
+        g.textAlign(PConstants.LEFT);
+        g.text(text, -textWidth / 2, textSize - 1);
+
+        g.popMatrix();
+    }
+
+    boolean isExpired() {
+        return System.currentTimeMillis() >= expireTime;
+    }
+}
