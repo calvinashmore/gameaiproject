368fa0d3808b94c99e9f8325a45db1bfcf556f7b
ken.a.hartsook <ken.a.hartsook@d15e2e6f-3fd1-c693-ec5a-e8bef22e7ec3>
2010-04-30 03:19:03 +0000

    - Refixed CharDev

--- trunk/src/testworld/game/CharDev.java
+++ trunk/src/testworld/game/CharDev.java
@@ -24,28 +24,28 @@ public class CharDev {
 
     public static void initializeCharacters() {
 
-        Cast.frank.addDependentAction(new ConversationDependentAction(CharDevConversations.meetFrank, "Introduce yourself", metFrank));
-        Cast.harriet.addDependentAction(new ConversationDependentAction(CharDevConversations.meetHarriet, "Introduce yourself", metHarriet));
-        Cast.hilda.addDependentAction(new ConversationDependentAction(CharDevConversations.meetHilda, "Introduce yourself", metHilda));
-        Cast.hughes.addDependentAction(new ConversationDependentAction(CharDevConversations.meetHughes, "Introduce yourself", metHughes));
-        Cast.victim.addDependentAction(new ConversationDependentAction(CharDevConversations.meetVictim, "Introduce yourself", metVictim));
-        Cast.gayle.addDependentAction(new ConversationDependentAction(CharDevConversations.meetGayle, "Introduce yourself", metGayle));
-        Cast.fred.addDependentAction((new ConversationDependentAction(CharDevConversations.meetFred, "Introduce yourself.", metFred)));
+        Cast.frank.addDependentAction(new ConversationDependentAction(CharDevConversations.meetFrank, "Introduce yourself"));
+        Cast.harriet.addDependentAction(new ConversationDependentAction(CharDevConversations.meetHarriet, "Introduce yourself"));
+        Cast.hilda.addDependentAction(new ConversationDependentAction(CharDevConversations.meetHilda, "Introduce yourself"));
+        Cast.hughes.addDependentAction(new ConversationDependentAction(CharDevConversations.meetHughes, "Introduce yourself"));
+        Cast.victim.addDependentAction(new ConversationDependentAction(CharDevConversations.meetVictim, "Introduce yourself"));
+        Cast.gayle.addDependentAction(new ConversationDependentAction(CharDevConversations.meetGayle, "Introduce yourself"));
+        Cast.fred.addDependentAction((new ConversationDependentAction(CharDevConversations.meetFred, "Introduce yourself.")));
         
-        Cast.frank.addDependentAction(new ConversationDependentAction(metFrank, CharDevConversations.frankOnHarriet, "Ask about Harriet"));
-        Cast.frank.addDependentAction(new ConversationDependentAction(metFrank, CharDevConversations.frankOnVictim, "Ask about Victim"));
-        Cast.harriet.addDependentAction(new ConversationDependentAction(metHarriet, CharDevConversations.harrietOnFrank, "Ask about Frank"));
-        Cast.harriet.addDependentAction(new ConversationDependentAction(metHarriet, CharDevConversations.harrietOnGayle, "Ask about Gayle"));
-        Cast.hughes.addDependentAction(new ConversationDependentAction(metHughes, CharDevConversations.hughesOnHilda, "Ask about Hilda"));
-        Cast.hughes.addDependentAction(new ConversationDependentAction(metHughes, CharDevConversations.hughesOnVictim, "Ask about Vicmti"));
-        Cast.hilda.addDependentAction(new ConversationDependentAction(metHilda, CharDevConversations.hildaOnHughes, "Ask about Hughes"));
-        Cast.hilda.addDependentAction(new ConversationDependentAction(metHilda, CharDevConversations.hildaOnFrank, "Ask about Frank"));
-        Cast.gayle.addDependentAction(new ConversationDependentAction(metGayle, CharDevConversations.gayleOnHughes, "Ask about Hughes"));
-        Cast.gayle.addDependentAction(new ConversationDependentAction(metGayle, CharDevConversations.gayleOnVictim, "Ask about Victim"));
-        Cast.victim.addDependentAction(new ConversationDependentAction(metVictim, CharDevConversations.victimOnGayle, "Ask about Gayle"));
-        Cast.victim.addDependentAction(new ConversationDependentAction(metVictim, CharDevConversations.victimOnHilda, "Ask about Hilda"));
+        Cast.frank.addDependentAction(new ConversationDependentAction(CharDevConversations.frankOnHarriet, "Ask about Harriet", metFrank));
+        Cast.frank.addDependentAction(new ConversationDependentAction(CharDevConversations.frankOnVictim, "Ask about Victim", metFrank));
+        Cast.harriet.addDependentAction(new ConversationDependentAction(CharDevConversations.harrietOnFrank, "Ask about Frank", metHarriet));
+        Cast.harriet.addDependentAction(new ConversationDependentAction(CharDevConversations.harrietOnGayle, "Ask about Gayle", metHarriet));
+        Cast.hughes.addDependentAction(new ConversationDependentAction(CharDevConversations.hughesOnHilda, "Ask about Hilda", metHughes));
+        Cast.hughes.addDependentAction(new ConversationDependentAction(CharDevConversations.hughesOnVictim, "Ask about Victim", metHughes));
+        Cast.hilda.addDependentAction(new ConversationDependentAction(CharDevConversations.hildaOnHughes, "Ask about Hughes", metHilda));
+        Cast.hilda.addDependentAction(new ConversationDependentAction(CharDevConversations.hildaOnFrank, "Ask about Frank", metHilda));
+        Cast.gayle.addDependentAction(new ConversationDependentAction(CharDevConversations.gayleOnHughes, "Ask about Hughes", metGayle));
+        Cast.gayle.addDependentAction(new ConversationDependentAction(CharDevConversations.gayleOnVictim, "Ask about Victim", metGayle));
+        Cast.victim.addDependentAction(new ConversationDependentAction(CharDevConversations.victimOnGayle, "Ask about Gayle", metVictim));
+        Cast.victim.addDependentAction(new ConversationDependentAction(CharDevConversations.victimOnHilda, "Ask about Hilda", metVictim));
 
-        Cast.fred.addDependentAction(new ConversationDependentAction(metFred, CharDevConversations.fredWantsSmoke, "Offer cigarette"));
+        Cast.fred.addDependentAction(new ConversationDependentAction(CharDevConversations.fredWantsSmoke, "Offer cigarette", metFred));
     }
 
 }
